75859cac7904ca55df152eb8e4ad0308c438c721
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-04-17 14:18:05 +0000

    Add 8-hbtn_class.js

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,23 @@
+export default class ALXClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('Size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError('Size must be a string');
+    }
+    this._size = size;
+    this._location = location;
+  }
+
+  //
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    if (hint === 'string') {
+      return this._location;
+    }
+    return this._location;
+  }
+}
