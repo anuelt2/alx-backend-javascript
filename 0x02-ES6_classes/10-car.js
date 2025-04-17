7e9823f4d6e37ad319566d5084c445cb304ac029
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-04-17 15:02:32 +0000

    Modify 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,23 +1,12 @@
-const cloneSymbol = Symbol('cloneCar');
-
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string' || typeof motor !== 'string' || typeof color !== 'string') {
-      throw new TypeError('Brand and motor and color must be strings');
-    }
-
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
   // Method that returns a new object of the class
-  [cloneSymbol]() {
-    const Cls = this.constructor;
-    return new Cls(this._brand, this._motor, this._color);
-  }
-
   cloneCar() {
-    return this[cloneSymbol]();
+    return new this.constructor(this._brand, this._motor, this.color);
   }
 }
