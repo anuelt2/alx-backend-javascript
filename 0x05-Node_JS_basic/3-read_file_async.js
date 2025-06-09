636bb8a1b6c2403320f0cbe6e0ef077dc369ce0b
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-06-09 16:37:54 +0000

    Add 3-read_file_async.js

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,39 @@
+const fs = require('node:fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n');
+      const students = lines.slice(1).filter(line => line.trim() !== '');
+
+      console.log(`Number of students: ${students.length}`);
+
+      const fields = {};
+
+      for (const line of students) {
+        const parts = line.split(',');
+        const firstname = parts[0];
+        const field = parts[parts.length - 1];
+
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      }
+
+      for (const field in fields) {
+        const list = fields[field].join(', ');
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${list}`)
+      }
+
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
