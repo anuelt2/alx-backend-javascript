14d903a530b29638cbf99c5e801b1f69b240c010
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-06-10 09:42:16 +0000

    Modify ALX to Holberton School in 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -44,7 +44,7 @@ const hostname = '127.0.0.1';
 const port = 1245;
 
 app.get('/', (req, res) => {
-  res.send('Hello ALX!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
