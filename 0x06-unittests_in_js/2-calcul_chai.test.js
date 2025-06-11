b6b31b10f4ae222ad999693736aa6e394352606f
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-06-11 19:00:47 +0000

    Add 2-calcul_chai.js 2-calcul_chai.test.js

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+const expect = chai.expect;
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', function() {
+  describe('SUM', function() {
+    it('should return 6 for sum of 1.4 and 4.5', function() {
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    });
+  });
+
+  describe('SUBTRACT', function() {
+    it('should return -4 for subtracting 4.5 from 1.4', function() {
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    });
+  });
+
+  describe('DIVIDE', function() {
+    it('should return 0.2 for dividing 1.4 by 4.5', function() {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    });
+
+    it('should return Error for dividing 1.4 by 0', function() {
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    });
+  });
+});
